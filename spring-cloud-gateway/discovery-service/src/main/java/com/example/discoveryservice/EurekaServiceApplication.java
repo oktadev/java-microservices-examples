871be5a25e6d648e69387a2984db058945458c1b
Matt Raible <matt.raible@okta.com>
2019-07-29 16:50:03 -0600

    Fix discovery

--- spring-cloud-gateway/discovery-service/src/main/java/com/example/discoveryservice/EurekaServiceApplication.java
+++ spring-cloud-gateway/discovery-service/src/main/java/com/example/discoveryservice/EurekaServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
