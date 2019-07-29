7bd1ab128b308d519e5c0180e225321c20f382b1
Matt Raible <matt.raible@okta.com>
2019-07-29 14:21:37 -0600

    Created projects

---
+++ spring-cloud-gateway/discovery-service/src/main/java/com/example/discoveryservice/EurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
